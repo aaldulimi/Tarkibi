85a08ad218aa0480a411b05252093c0ef7884267
Ahmed <ahmedamedo22@gmail.com>
2023-09-21 21:16:43 +1000

    diarization and noise reduction processes have their own classes now

--- tarkibi/dataset.py
+++ tarkibi/dataset.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 import os
 import tarkibi.utilities.general, tarkibi.utilities.youtube, tarkibi.utilities.audio, tarkibi.utilities.agent
 import shutil
+import typing
 
 AUDIO_NN = tarkibi.utilities.general.AUDIO_NN
 AUDIO_RAW = tarkibi.utilities.general.AUDIO_RAW
@@ -12,71 +13,111 @@ AUDIO_SPECS = tarkibi.utilities.general.AUDIO_SPECS
 AUDIO_FINAL = tarkibi.utilities.general.AUDIO_FINAL
 BASE_DIR = tarkibi.utilities.general.BASE_DIR
 
-def noise_reduction(file_path: str) -> None:
-    output_path = f'{BASE_DIR}/{AUDIO_NN}'
-    subprocess.run(f'spleeter separate -o {output_path} {file_path}', shell=True)
-   
-def diarize_audio(file_path: str, audio_id: str) -> None:    
-    diar = Diarizer(
-      embed_model='xvec',  
-      cluster_method='sc' 
-    )
 
-    segments = diar.diarize(file_path, 
-        num_speakers=None,
-        threshold=1e-1,
-    )
+# restructure project
 
-    speakers = {}
-    for segment in segments:
-        speaker_id = segment['label']
-        segment_start = segment['start']
-        segment_end = segment['end']
+class Tarkibi:
+    def __init__(self):
+        pass
 
-        if speaker_id not in speakers:
-            speakers[speaker_id] = {
-                'duration': segment_end - segment_start, 
-                'segments': [{
+    def build_dataset(self):
+        pass
+
+    def process_video(self):
+        pass
+
+
+class _NoiseReduction:
+    _NR_OUTPUT_PATH = f'{BASE_DIR}/{AUDIO_NN}'
+
+    def __init__(self):
+        pass
+        
+    def _noise_reduction(self, file_path: str = None) -> None:
+        subprocess.run(f'spleeter separate -o {self._NR_OUTPUT_PATH} {file_path}', shell=True)
+
+class _Diarization:
+    _AUDIO_CLIPS_PATH = f'{BASE_DIR}/{AUDIO_CLIPS}'
+
+    def __init__(self):
+        pass
+        
+    def _diarize_audio_to_segments(self, file_path: str = None) -> list | dict[str, typing.Any]:
+        diar = Diarizer(
+            embed_model='xvec',  
+            cluster_method='sc' 
+        )
+
+        segments = diar.diarize(
+            file_path, 
+            num_speakers=None,
+            threshold=1e-1,
+        )
+        
+        return segments
+
+    def _group_segments_by_speaker(self, segments: list | dict[str, typing.Any]) -> dict[str, typing.Any]:
+        speakers = {}
+        for segment in segments:
+            speaker_id = segment['label']
+            segment_start = segment['start']
+            segment_end = segment['end']
+
+            if speaker_id not in speakers:
+                speakers[speaker_id] = {
+                    'duration': segment_end - segment_start, 
+                    'segments': [{
+                        'start': segment_start,
+                        'end': segment_end
+                    }]
+                }
+            
+            else:
+                speakers[speaker_id]['duration'] += segment_end - segment_start
+                speakers[speaker_id]['segments'].append({
                     'start': segment_start,
                     'end': segment_end
-                }]
-            }
+                })
         
-        else:
-            speakers[speaker_id]['duration'] += segment_end - segment_start
-            speakers[speaker_id]['segments'].append({
-                'start': segment_start,
-                'end': segment_end
-            })
+        return speakers
     
 
-    for speaker, info in speakers.items():
-        if not os.path.exists(f'{BASE_DIR}/{AUDIO_CLIPS}/{audio_id}'):
-            os.mkdir(f'{BASE_DIR}/{AUDIO_CLIPS}/{audio_id}')
-            
-        i = 0
-        for segment in info['segments']:
-            segment_start = tarkibi.utilities.general.format_time(segment['start'])
-            segment_end = tarkibi.utilities.general.format_time(segment['end']) 
+    def _segment_audio_clips(self, speakers: dict[str, typing.Any], file_path: str, audio_id: str) -> None:
+        for speaker, info in speakers.items():
+            audio_clips_path = f'{self._AUDIO_CLIPS_PATH}/{audio_id}'
 
-            subprocess.run(
-                f'ffmpeg -i {file_path} -ss {segment_start} -to {segment_end} -c copy {BASE_DIR}/{AUDIO_CLIPS}/{audio_id}/{speaker}_{i}.wav',
-                shell=True,
-                stdout=subprocess.DEVNULL
-            )
+            if not os.path.exists(audio_clips_path):
+                os.mkdir(audio_clips_path)
+                
+            i = 0
+            for segment in info['segments']:
+                segment_start = tarkibi.utilities.general.format_time(segment['start'])
+                segment_end = tarkibi.utilities.general.format_time(segment['end']) 
 
-            i += 1
+                subprocess.run(
+                    f'ffmpeg -i {file_path} -ss {segment_start} -to {segment_end} -c copy {audio_clips_path}/{speaker}_{i}.wav',
+                    shell=True,
+                    stdout=subprocess.DEVNULL
+                )
+
+                i += 1
     
-def process_video(video_id: str, debug_mode: bool = False):
-    try:
-        tarkibi.utilities.youtube.download_video(video_id, output_path=f'{BASE_DIR}/{AUDIO_RAW}')
-    except:
-        return
+    def _diarize_audio(self, file_path: str, audio_id: str) -> None:
+        segments = self._diarize_audio_to_segments(file_path)
+        speakers = self._group_segments_by_speaker(segments)
+        self._segment_audio_clips(speakers, file_path, audio_id)
+
     
-    noise_reduction(f'{BASE_DIR}/{AUDIO_RAW}/{video_id}.wav')
-    diarize_audio(f'{BASE_DIR}/{AUDIO_NN}/{video_id}/vocals.wav', video_id)
+# def process_video(video_id: str, debug_mode: bool = False):
+#     try:
+#         tarkibi.utilities.youtube.download_video(video_id, output_path=f'{BASE_DIR}/{AUDIO_RAW}')
+#     except:
+#         return
     
-    if not debug_mode: tarkibi.utilities.general.light_clean(video_id)
+#     noise_reduction(f'{BASE_DIR}/{AUDIO_RAW}/{video_id}.wav')
+#     diarize_audio(f'{BASE_DIR}/{AUDIO_NN}/{video_id}/vocals.wav', video_id)
+    
+#     if not debug_mode: tarkibi.utilities.general.light_clean(video_id)
      
      
 def build_dataset(author: str, reference_audio: str, target_duration: timedelta, output_path: str = 'dataset', sample_rate: int = 24000,
