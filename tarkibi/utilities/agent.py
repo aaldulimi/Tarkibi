f78c4afc514368b81b0791f19d0549f63fcfe7bf
Ahmed <ahmedamedo22@gmail.com>
2023-09-26 00:03:20 +1000

    switch agent model to gpt4

--- tarkibi/utilities/agent.py
+++ tarkibi/utilities/agent.py
@@ -15,7 +15,7 @@ class _Agent:
         # use instruct or functions in the future
         search_query = self._using_chat_completion(person)
         logger.info(f'Tarkibi _generate_search_query: Generated search query: {search_query}')
-        
+
         return search_query
 
     def _using_chat_completion(self, person: str) -> str:
@@ -23,7 +23,7 @@ class _Agent:
         Generate a search query for a person
         """
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4",
             messages=[
                 {"role": "system", "content": "Think step by step. Im building an audio dataset for one person. This person is Joe Rogan. I'm using youtube as the source for my audio clips (before pre-processing...). What should the youtube search query be? Return only the search query.  Dont add any other input. Just on string, which is the search query. Thank you."},
                 {"role": "user", "content": "Joe Rogan"},
